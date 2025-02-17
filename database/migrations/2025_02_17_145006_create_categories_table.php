a38cac13db4931fead04812e5f01946c2a21b115
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-17 17:11:56 +0100

    KAN-24 KAN-25 KAN-41 creer table categories

---
+++ database/migrations/2025_02_17_145006_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("CREATE TYPE categorie_type AS ENUM ('Iftar', 'Suhoor', 'Desserts')");
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->enum('nom', ['Iftar', 'Suhoor', 'Desserts'])->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+        
+        DB::statement("DROP TYPE IF EXISTS categorie_type");
+    }
+};
