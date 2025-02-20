5671c86a3a55e519064c159b8d82512b09f8c025
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-20 15:11:03 +0100

    KAN-35 creer formulaire pour ajoutée nouvel temoignange

--- resources/views/pages/temoignages.blade.php
+++ resources/views/pages/temoignages.blade.php
@@ -15,7 +15,7 @@
                         <p class="text-black mb-4">{{ Str::limit($temoignage->contenu, 100) }}</p>
                         <div class="flex items-center justify-between">
                             <div class="flex items-center">
-                                <img src="{{ $temoignage->user_avatar }}" class="w-8 h-8 rounded-full" alt="Author">
+                                <img src="https://freesvg.org/img/publicdomainq-0006224bvmrqd.png" class="w-8 h-8 rounded-full" alt="Author">
                                 <span class="ml-2 text-sm text-gray-600">{{ $temoignage->nom }}</span>
                             </div>
                             <div class="flex items-center space-x-4">
