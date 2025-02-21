3d704dd0a5fa1f3ab7fe17256144f3a08041faa3
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-21 05:49:49 +0100

    KAN-13 KAN-30 implementer la vreation de recettes et chsoisir categorie corespondant et choissir categorie depuis base de donnees

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -25,6 +25,24 @@ class RecetteController extends Controller
         return view('pages.recettes', compact('recettes', 'categories', 'categorie'));
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|string|max:255',
+        'titre' => 'required|string|max:255',
+        'categorie_id' => 'required|exists:categories,id',
+        'duree' => 'required|integer|min:1',
+        'description' => 'required|string',
+        'ingredient' => 'nullable|string',
+        'instructions' => 'nullable|string',
+        'image_url' => 'required|url|max:255',
+        ]);
+
+        Recette::create($request->all());
+
+        return redirect()->route('recettes.index')->with('success', 'Recette ajoutée avec succès !');
+    }
+
     // public function show($id) {
     //     $recette = Recette::findOrFail($id);
     //     return view('pages.recette_detail', compact('recette'));
