c5b58550967877b508290fa871ce6761bd5bc0fb
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-21 04:50:38 +0100

    KAN-36 afficher les recettes avec filtre

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,12 @@ Route::get('/categories', [CategorieController::class, 'index']);
 Route::get('/temoignages', [TemoignageController::class, 'index']);
 Route::post('/temoignages', [TemoignageController::class, 'store'])->name('temoignages.store');
 
-Route::get('/recettes', [RecetteController::class, 'index']);
+// Route::get('/recettes', [RecetteController::class, 'index']);
 // Route::post('/recettes', [RecetteController::class, 'store']);
 
 
 Route::get('/recettes', [RecetteController::class, 'index'])->name('recettes.index');
-Route::get('/recettes/{id}', [RecetteController::class, 'show'])->name('recettes.show');
+// Route::get('/recettes/{id}', [RecetteController::class, 'show'])->name('recettes.show');
 
-Route::post('/commentaires', [CommentaireController::class, 'store'])->name('commentaires.store');
-Route::get('/commentaires/{temoignage}', [CommentaireController::class, 'getComments']);
+Route::post('/commentaires/store', [CommentaireController::class, 'store'])->name('commentaires.store');
+// Route::get('/commentaires/{temoignage}', [CommentaireController::class, 'getComments']);
