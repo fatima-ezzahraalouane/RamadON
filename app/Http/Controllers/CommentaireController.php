32b34a1def71a74275ceae153d0d7e4e4f8e81dc
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-21 11:57:03 +0100

    KAN-17 KAN-31 KAN-39 KAN-18 modifier le fichier controller pour gerer aussi les commentaires dans les recettes

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Commentaire;
 use App\Models\Temoignage;
+use App\Models\Recette;
 
 class CommentaireController extends Controller
 {
@@ -13,13 +14,15 @@ class CommentaireController extends Controller
         $request->validate([
             'nom' => 'required|string|max:255',
             'contenu' => 'required|string',
-            'temoignage_id' => 'required|exists:temoignages,id',
+            'temoignage_id' => 'nullable|exists:temoignages,id',
+            'recette_id' => 'nullable|exists:recettes,id',
         ]);
     
         Commentaire::create([
             'nom' => $request->nom,
             'contenu' => $request->contenu,
-            'temoignage_id' => $request->temoignage_id,
+            'temoignage_id' => $request->temoignage_id ?? null,
+            'recette_id' => $request->recette_id ?? null,
         ]);
     
         return redirect()->back()->with('success', 'Votre commentaire a été publié avec succès.');
