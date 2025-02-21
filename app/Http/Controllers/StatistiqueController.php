c67a5688cbca6094aff2a0c1641a0abe5d0139aa
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-21 14:28:41 +0100

    KAN-20 KAN-21 KAN-22 KAN-40 KAN-32 creer fonction index pour compter les donnees necissaire

--- app/Http/Controllers/StatistiqueController.php
+++ app/Http/Controllers/StatistiqueController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Temoignage;
+use App\Models\Recette;
+use App\Models\Commentaire;
 
 class StatistiqueController extends Controller
 {
-    //
+    public function index()
+    {
+        $totalRecettes = Recette::count();
+        $totalTemoignages = Temoignage::count();
+        $totalCommentairesRecettes = Commentaire::whereNotNull('recette_id')->count();
+        $totalCommentairesTemoignages = Commentaire::whereNotNull('temoignage_id')->count();
+
+        $topRecettes = Recette::withCount('commentaires')
+            ->orderByDesc('commentaires_count')
+            ->take(3)
+            ->get();
+
+        return view('pages.statistiques', compact(
+            'totalRecettes',
+            'totalTemoignages',
+            'totalCommentairesRecettes',
+            'totalCommentairesTemoignages',
+            'topRecettes'
+        ));
+    }
 }
